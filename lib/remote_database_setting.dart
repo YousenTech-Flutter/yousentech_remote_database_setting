e1729eb9e37f6ebc300b9fe664b093eebc2c8e32
mohmmedsaleh <mohmmed123232@gmail.com>
2025-01-22 15:20:13 +0300

    update

--- lib/remote_database_setting.dart
+++ lib/remote_database_setting.dart
@@ -1,7 +1,7 @@
 library remote_database_setting;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
