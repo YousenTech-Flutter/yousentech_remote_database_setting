3931c53c89ab934f4edfbe67b46bc3aebe4f27f4
eemzain <hudazain55@gmail.com>
2025-12-08 10:44:44 +0300

    .

--- lib/remote_database_setting/presentation/create_support_ticket.dart
+++ lib/remote_database_setting/presentation/create_support_ticket.dart
@@ -56,7 +56,9 @@ supportTicketDialog({
                             'support_ticket'.tr,
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              color: SharedPr.isDarkMode! ?Colors.white : const Color(0xFF2E2E2E),
+                              color: SharedPr.isDarkMode!
+                                  ? Colors.white
+                                  : const Color(0xFF2E2E2E),
                               fontSize: context.setSp(20.03),
                               fontFamily: 'Tajawal',
                               fontWeight: FontWeight.w700,
@@ -68,12 +70,13 @@ supportTicketDialog({
                             size: context.setMinSize(40),
                           ),
                           SizedBox(height: context.setHeight(10)),
-                      
                           Text(
                             'exception_details_message'.tr,
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              color:SharedPr.isDarkMode! ? Color(0xFFB1B3BC) :  const Color(0xFF9F9FA5),
+                              color: SharedPr.isDarkMode!
+                                  ? const Color(0xFFB1B3BC)
+                                  : const Color(0xFF9F9FA5),
                               fontSize: context.setSp(14.42),
                               fontFamily: 'Tajawal',
                               fontWeight: FontWeight.w400,
@@ -82,120 +85,132 @@ supportTicketDialog({
                           SizedBox(height: context.setHeight(10)),
                           ContainerTextField(
                             controller: exceptionDetails,
-                            keyboardType : TextInputType.text,
+                            keyboardType: TextInputType.text,
                             width: context.screenWidth,
                             height: context.setHeight(51.28),
                             fontSize: context.setSp(11.22),
                             maxLines: 6,
                             enabled: message == null ? true : false,
                             isAddOrEdit: true,
-                            borderColor: AppColor.silverGray,
-                            iconcolor: AppColor.silverGray,
-                            hintcolor: AppColor.silverGray,
+                            borderColor: !SharedPr.isDarkMode!
+                                ? const Color(
+                                    0xFFC2C3CB,
+                                  )
+                                : null,
+                            fillColor: !SharedPr.isDarkMode!
+                                ? Colors.white.withValues(
+                                    alpha: 0.43,
+                                  )
+                                : const Color(
+                                    0xFF2B2B2B,
+                                  ),
+                            hintcolor: !SharedPr.isDarkMode!
+                                ? const Color(
+                                    0xFFC2C3CB,
+                                  )
+                                : const Color(
+                                    0xFFC2C3CB,
+                                  ),
+                            color: !SharedPr.isDarkMode!
+                                ? const Color(
+                                    0xFFC2C3CB,
+                                  )
+                                : const Color(
+                                    0xFFC2C3CB,
+                                  ),
                             borderRadius: context.setMinSize(5),
                             textAlign: TextAlign.start,
-                            
                             hintText: 'exception_details'.tr,
                             labelText: 'exception_details'.tr,
-                                            
                             validator: (value) {
                               if (value == null || value.isEmpty) {
-                                errorMessage = 'required_message_f'
-                                    .trParams({
-                                      'field_name':
-                                          'exception_details'.tr,
-                                    });
+                                errorMessage = 'required_message_f'.trParams({
+                                  'field_name': 'exception_details'.tr,
+                                });
                                 countErrors++;
                                 return "";
                               }
                               return null;
                             },
                           ),
-                          Spacer()     ,                   
+                          Spacer(),
                           Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                ButtonElevated(
-                                  text:
-                                      message != null
-                                          ? 'yes'.tr
-                                          : 'send_ticket'.tr,
-                                  backgroundColor: AppColor.cyanTeal,
-                                  height: context.setHeight(35),
-                                  width:context.setWidth(180) ,
-                                  borderRadius: context.setMinSize(5),
-                                  textStyle: AppStyle.textStyle(
-                                    color: AppColor.white,
-                                    fontSize: context.setSp(12),
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Tajawal',
-                                  ),
-                                  onPressed:
-                                      onPressed ??
-                                      () async {
-                                        countErrors = 0;
-                                        if (_formKey.currentState!.validate()) {
-                                          await databaseSettingController
-                                              .sendTicket(
-                                                subscriptionId:
-                                                    SharedPr
-                                                        .subscriptionDetailsObj!
-                                                        .subscriptionId
-                                                        .toString(),
-                                                message: exceptionDetails.text,
-                                              )
-                                              .then((value) {
-                                                if (value.status) {
-                                                  SharedPr.setNotificationObj(
-                                                    notificationHelperObj:
-                                                        NotificationHelper(
-                                                          sendTicket: true,
-                                                        ),
-                                                  );
-                                                  Get.back();
-                                                  appSnackBar(
-                                                    message:
-                                                        'success_send_ticket'.tr,
-                                                    messageType:
-                                                        MessageTypes.success,
-                                                  );
-                                                  exceptionDetails.clear();
-                                                } else {
-                                                  appSnackBar(
-                                                    message: value.message!,
-                                                  );
-                                                  exceptionDetails.clear();
-                                                }
-                                              });
-                                        } else {
-                                          appSnackBar(
-                                            message:
-                                                countErrors > 1
-                                                    ? 'enter_required_info'.tr
-                                                    : errorMessage!,
-                                          );
-                                        }
-                                      },
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ButtonElevated(
+                                text: message != null
+                                    ? 'yes'.tr
+                                    : 'send_ticket'.tr,
+                                backgroundColor: AppColor.cyanTeal,
+                                height: context.setHeight(35),
+                                width: context.setWidth(180),
+                                borderRadius: context.setMinSize(5),
+                                textStyle: AppStyle.textStyle(
+                                  color: AppColor.white,
+                                  fontSize: context.setSp(12),
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Tajawal',
                                 ),
-                                ButtonElevated(
-                                  text: 'cancel'.tr,
-                                  height: context.setHeight(35),
-                                  width:context.setWidth(180),
-                                  borderColor: AppColor.paleAqua,
-                                  borderRadius: context.setMinSize(5),
-                                  textStyle: AppStyle.textStyle(
-                                    color: AppColor.slateGray,
-                                    fontSize: context.setSp(12),
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Tajawal',
-                                  ),
-                                  onPressed: () async {
-                                    Get.back();
-                                  },
+                                onPressed: onPressed ??
+                                    () async {
+                                      countErrors = 0;
+                                      if (_formKey.currentState!.validate()) {
+                                        await databaseSettingController
+                                            .sendTicket(
+                                          subscriptionId: SharedPr
+                                              .subscriptionDetailsObj!
+                                              .subscriptionId
+                                              .toString(),
+                                          message: exceptionDetails.text,
+                                        )
+                                            .then((value) {
+                                          if (value.status) {
+                                            SharedPr.setNotificationObj(
+                                              notificationHelperObj:
+                                                  NotificationHelper(
+                                                sendTicket: true,
+                                              ),
+                                            );
+                                            Get.back();
+                                            appSnackBar(
+                                              message: 'success_send_ticket'.tr,
+                                              messageType: MessageTypes.success,
+                                            );
+                                            exceptionDetails.clear();
+                                          } else {
+                                            appSnackBar(
+                                              message: value.message!,
+                                            );
+                                            exceptionDetails.clear();
+                                          }
+                                        });
+                                      } else {
+                                        appSnackBar(
+                                          message: countErrors > 1
+                                              ? 'enter_required_info'.tr
+                                              : errorMessage!,
+                                        );
+                                      }
+                                    },
+                              ),
+                              ButtonElevated(
+                                text: 'cancel'.tr,
+                                height: context.setHeight(35),
+                                width: context.setWidth(180),
+                                borderColor: AppColor.paleAqua,
+                                borderRadius: context.setMinSize(5),
+                                textStyle: AppStyle.textStyle(
+                                  color: AppColor.slateGray,
+                                  fontSize: context.setSp(12),
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Tajawal',
                                 ),
-                              ],
-                            ),
-                      
+                                onPressed: () async {
+                                  Get.back();
+                                },
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
